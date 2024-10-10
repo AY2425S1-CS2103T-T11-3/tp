1134b30a809194116c78ec01f580114b8e4a61cb
jowhee3011 <joeygoh3011@gmail.com>
2024-10-10 23:29:04 +0800

    edit checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.ALICE;
+//import static seedu.address.testutil.TypicalPersons.ALICE;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -82,7 +82,7 @@ public class AddCommandTest {
 //        AddCommand addCommand = new AddCommand(ALICE);
 //        String expected = AddCommand.class.getCanonicalName() + "{toAdd=" + ALICE + "}";
 //        assertEquals(expected, addCommand.toString());
-//    }
+//        }
 
     /**
      * A default model stub that have all of the methods failing.
