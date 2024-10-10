8bbbdb3f44e14a7a233af441f18fffcb44916f09
jowhee3011 <joeygoh3011@gmail.com>
2024-10-10 23:53:40 +0800

    edit checkstyle

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
-//import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -77,12 +77,12 @@ public class AddCommandTest {
         assertFalse(addAliceCommand.equals(addBobCommand));
     }
 
-//    @Test
-//    public void toStringMethod() {
-//        AddCommand addCommand = new AddCommand(ALICE);
-//        String expected = AddCommand.class.getCanonicalName() + "{toAdd=" + ALICE + "}";
-//        assertEquals(expected, addCommand.toString());
-//  }
+    //    @Test
+    //    public void toStringMethod() {
+    //        AddCommand addCommand = new AddCommand(ALICE);
+    //        String expected = AddCommand.class.getCanonicalName() + "{toAdd=" + ALICE + "}";
+    //        assertEquals(expected, addCommand.toString());
+    //    }
 
     /**
      * A default model stub that have all of the methods failing.
